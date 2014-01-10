c6944612597e69b3b62a7bf22753324b2d0881f4
Ozair Nazir <ozair.nazir@riksof.com>
2014-01-10 19:53:42 +0500

    fixed data insertion in models

--- cloud/cloud/main.js
+++ cloud/cloud/main.js
@@ -1,4 +1,3 @@
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("mergeUser", function(request, response) {
@@ -10,47 +9,115 @@ Parse.Cloud.define("mergeUser", function(request, response) {
 
   // Set your id to desired user object id
   oUser.id = request.params.oldUser;
+  var newUser = request.params.newUser;
+
+/*var inapps = new Parse.Query("InApp");
+
+      		inapps.equalTo("user", oUser);
+
+      		inapps.find().then(function(results) {
+
+			if (status != -1) {
+             			status = status + ' AND ';
+          		}
+			status = status + "Found " + results.length + " in apps for userId " + request.params.oldUser;
+			
+			var InAppR = Parse.Object.extend("InAppR");
+
+			// this will store the rows for use with Parse.Object.saveAll
+			var inAppArray = [];
+
+			// create objects,
+			for (var i = 0; i < results.length ; i++) { 
+			      var newInApp = new InAppR();
+			      newInApp.set("user", newUser);
+			      newInApp.set("json", results[i].json);
+			      inAppArray.push(newInApp);
+			}
+
+			Parse.Object.saveAll(inAppArray).then(function(){
+			
+				if (status != -1) {
+             				status = status + ' AND ';
+          			}
+				
+response.success(status);
+		  			
+			});
+//response.success(status + " test ="+newInApp.get("user"));
+});
+*/
+
+
+
+
 
   var flyers = new Parse.Query("Flyer");
 
   flyers.equalTo("user", oUser);
 
-  flyers.find({
-    success: function(results) {
-  
-      status = "Found " + results.length + " flyers for userId " + request.params.oldUser;
+  flyers.find().then(function(results) {
 
-    },
+	var FlyerTest = Parse.Object.extend("FlyerTest");
 
-    error: function() {
-   
-      status = "Error in query finding flyers for user " + request.params.oldUser; 
-      response.error(status);
-    }
-  }).then(;
+        // this will store the rows for use with Parse.Object.saveAll
+        var flayerArray = [];
 
-  var inapps = new Parse.Query("InApp");
+      	status = "Found " + results.length + " flyers for userId " + request.params.oldUser;
+	//status = "Found =" + results[i].file + " date = " + results[i].createdAt ;
 
-  inapps.equalTo("user", oUser);
+        // create objects,
+        for (var i = 1; i < results.length ; i++) { 
+          var newFlayer = new FlyerTest();
+          newFlayer.set("user", newUser);
+          //newFlayer.set("image", results[i].image);
+          flayerArray.push(newFlayer);
+        }
 
-  inapps.find({
-    success: function(results) {
+        // save all the newly created objects
+        Parse.Object.saveAll(flayerArray).then(function() {
+	
+		var inapps = new Parse.Query("InApp");
+
+      		inapps.equalTo("user", oUser);
+
+      		inapps.find().then(function(results) {
+
+			if (status != -1) {
+             			status = status + ' AND ';
+          		}
+
+			status = status + "Found " + results.length + " in apps for userId " + request.params.oldUser;
+			
+			var InAppR = Parse.Object.extend("InAppR");
+
+			// this will store the rows for use with Parse.Object.saveAll
+			var inAppArray = [];
+
+			// create objects,
+			for (var i = 0; i < results.length ; i++) { 
+			      var newInApp = new InAppR();
+			      newInApp.set("user", newUser);
+			      newInApp.set("json", results[i].json);
+			      inAppArray.push(newInApp);
+			}
+
+			Parse.Object.saveAll(inAppArray).then(function(){
+			
+				if (status != -1) {
+             				status = status + ' AND ';
+          			}
+
+		  		response.success(status);	
+			});
+			
+     			//response.success(status);
+		});
+			
+	
+	});
       
-      if (status != -1) {
-         status = status + ' AND ';
-      }
- 
-      status = status + "Found " + results.length + " in apps for userId " + request.params.oldUser;
-      
-    },
-
-    error: function() {
-      
-      status = "Error in query finding in apps for user " + request.params.oldUser; 
-      response.error(status);
-    }
+      //response.success(status);
   });
-
-  response.success(status);
 
 });
