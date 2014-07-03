aeee0e432c26b811144351298cefb78ca5cae2f2
Abdul Rauf <abdul.rauf@riskof.com>
2014-07-03 10:57:29 +0500

    issue #114 fixed(By arbab)

--- Classes/SigninController.m
+++ Classes/SigninController.m
@@ -383,6 +383,9 @@
         }else{
             registerController = [[RegisterController alloc]initWithNibName:@"RegisterController" bundle:nil];
         }
+        
+        registerController.signInController = self;
+        
         [self.navigationController pushViewController:registerController animated:YES];
         
     }
