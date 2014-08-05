d4238d0bab25812a3344a06cbd6164fbd26f94fe
Muhammad Arbab <abdul.rauf@riskof.com>
2014-08-05 15:06:34 +0500

    Added UI for BuyImagesViewControllerClasses/AssetGroupViewControllerWithSearchFeild.h

---
+++ Classes/AssetGroupViewControllerWithSearchFeild.m
@@ -0,0 +1,38 @@
+//
+//  AssetGroupViewControllerWithSearchFeild.m
+//  Flyr
+//
+//  Created by RIKSOF Developer on 8/5/14.
+//
+//
+
+#import "AssetGroupViewControllerWithSearchFeild.h"
+
+@interface AssetGroupViewControllerWithSearchFeild ()
+
+@end
+
+@implementation AssetGroupViewControllerWithSearchFeild
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
