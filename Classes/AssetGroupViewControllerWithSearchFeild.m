5db89bc3c4d503dc76650f24bc2ccc93487f6594
Muhammad Arbab <abdul.rauf@riskof.com>
2014-08-07 14:49:55 +0500

    Small UI fixes related to BuyImagesController

--- Classes/AssetGroupViewControllerWithSearchFeild.m
+++ Classes/AssetGroupViewControllerWithSearchFeild.m
@@ -7,6 +7,7 @@
 //
 
 #import "AssetGroupViewControllerWithSearchFeild.h"
+#import "Common.h"
 
 @interface AssetGroupViewControllerWithSearchFeild ()
 
@@ -32,6 +33,26 @@
     // Configure the grid view
     //self.gridView.margin = CGSizeMake(5.0, 5.0);
     //self.gridView.nibNameForViews = @"CustomAssetThumbnailView";
+    
+    // Navigation buttons
+    // BackButton
+    UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 45, 42)];
+    backButton.titleLabel.font = [UIFont systemFontOfSize:14.0];
+    [backButton addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    [backButton setBackgroundImage:[UIImage imageNamed:@"back_button"] forState:UIControlStateNormal];
+    backButton.showsTouchWhenHighlighted = YES;
+    UIBarButtonItem *leftBarButton = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+    [self.navigationItem setLeftBarButtonItem:leftBarButton];
+    
+    
+    // Set the title view.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(-35, -6, 50, 50)];
+    label.backgroundColor = [UIColor clearColor];
+    label.font = [UIFont fontWithName:TITLE_FONT size:18];
+    label.textColor = [UIColor colorWithRed:0 green:155.0/255.0 blue:224.0/255.0 alpha:1.0];
+    label.text = @"Stock Photos";
+    
+    self.navigationItem.titleView = label;
 }
 
 - (void)didReceiveMemoryWarning
