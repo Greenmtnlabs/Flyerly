fd48f425f87f8485a1980e8ac7df2c6dbd185b43
Zeeshan Lalani <riksof-zzlalani@github.com>
2014-10-31 17:23:22 +0500

    maded sone changes in reset db api

--- server/providers/ResetDb.js
+++ server/providers/ResetDb.js
@@ -1,6 +1,6 @@
 /**
  * This module is responsible for removing Events, and setting Twillio numbers to FREE
- * 
+ *
  * Developer: Muhammad Zeeshan
  */
 // Reference to the module to be exported
@@ -21,44 +21,66 @@ ResetDb.setup = function(app) {
     app.get('/reset-db', function(req, res) {
         var Twillio = require(__dirname + '/../models/Twillio');
         var Events = require(__dirname + '/../models/Events');
-        
-		// remove all events except admin default event
-		Events.remove({
+
+        // remove all events except admin default event
+        Events.find({
                 recording: {
                     $ne: "DEFAULT1.wav"
                 }
             },
-            function(err, removed) {
+            function(err, events) {
                 if (err) {
                     logger.error(JSON.stringify(err));
                     return;
                 }
-                
-				logger.info('Total ' + removed + ' Events removed successfully.');
 
-                // update twillio objects
-                Twillio.update({
-                        status: "IN_USE"
-                    }, {
-                        status: "FREE"
-                    }, {
-                        safe: true
+                var fs = require('fs');
+
+                for (var i = 0; i < events.length; i++) {
+					// remove recording files
+                    fs.unlinkSync(__dirname + '/../../recordings/' + events[i].recording);
+					logger.info('File ' + events[i].recording + ' removed successfully.');
+                }
+
+				// remove events
+                Events.remove({
+                        recording: {
+                            $ne: "DEFAULT1.wav"
+                        }
                     },
                     function(err, count) {
-
                         if (err) {
                             logger.error(JSON.stringify(err));
                             return;
                         }
 
-                        logger.info('Total ' + count + ' twillio updated successfully.');
-                        res.json(200, {
-                            status: "OK"
-                        });
+                        logger.info('Total ' + count + ' Events removed successfully.');
 
-                    }); // updated end
+                        // update twillio objects
+                        Twillio.update({
+                                status: "IN_USE"
+                            }, {
+                                status: "FREE"
+                            }, {
+                                safe: true
+                            },
+                            function(err, count) {
 
-            }); // delete end
+                                if (err) {
+                                    logger.error(JSON.stringify(err));
+                                    return;
+                                }
+
+                                logger.info('Total ' + count + ' twillio updated successfully.');
+                                res.json(200, {
+                                    status: "OK"
+                                });
+
+                            }); // updated end
+
+                    }); // remove end
+
+            }); // find end
 
 
     });
