eb15c48d9e5e9349f9352c3ac27b98fccec51be0
riksof-abdulrauf <abdul.rauf@riksof.com>
2014-11-18 15:56:48 +0500

    __line issue fix

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+//We can track the line number using this code____{_________
+Object.defineProperty(global, '__stack', {
+  get: function(){
+    var orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = function(_, stack){ return stack; };
+    var err = new Error;
+    Error.captureStackTrace(err, arguments.callee);
+    var stack = err.stack;
+    Error.prepareStackTrace = orig;
+    return stack;
+  }
+});
+
+Object.defineProperty(global, '__line', {
+  get: function(){
+    return __stack[1].getLineNumber();
+  }
+});
+	//Use bellow code for use	
+	//console.log(__stack);
+	//console.log(__line);
+//We can track the line number using this code____}_________
+	
 // Get the configurations
 var config = require( __dirname + '/config' );
 
