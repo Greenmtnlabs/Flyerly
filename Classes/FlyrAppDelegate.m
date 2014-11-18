03ed9cd650f0a662d883f8e846fc4866f847cb39
Muhammad Arbab <abdul.rauf@riskof.com>
2014-11-18 12:56:14 +0500

    remote notifications warning fixed

--- Classes/FlyrAppDelegate.m
+++ Classes/FlyrAppDelegate.m
@@ -182,6 +182,21 @@ NSString *FacebookDidLoginNotification = @"FacebookDidLoginNotification";
     
     [LobRequest initWithAPIKey:[flyerConfigurator lobAppId]];
     
+    //-- Set Notification
+    if ([application respondsToSelector:@selector(isRegisteredForRemoteNotifications)])
+    {
+        // iOS 8 Notifications
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        
+        [application registerForRemoteNotifications];
+    }
+    else
+    {
+        // iOS < 8 Notifications
+        [application registerForRemoteNotificationTypes:
+         (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)];
+    }
+    
 #ifdef DEBUG
     
     // Setup parse Offline ozair's account
