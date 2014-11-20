701e8f7b54544b2a867027b29030e712078b9214
riksof-abdulrauf <abdul.rauf@riksof.com>
2014-11-20 21:51:58 +0500

    post on linkedin done, continue work for update posting flag

--- server/providers/PostSocialStatusCron.js
+++ server/providers/PostSocialStatusCron.js
@@ -22,7 +22,7 @@ SocialStatusCron.setup = function(app) {
 	var twitter = require('ntwitter');
 	
 	var https = require('https');
-	var request = require('request');
+	//var request = require('request');
 	
     // Get today's date
     var today = new Date();
@@ -38,7 +38,7 @@ SocialStatusCron.setup = function(app) {
 	
 	function logMsg( msg ) {
 		logger.info( msg );
-		console.log( msg );				
+		//console.log( msg );				
 	}
 	
     // Check if the event is expire
@@ -60,61 +60,72 @@ SocialStatusCron.setup = function(app) {
         }, function(err, events) {
             
 			if (err) {
-                logMsg( JSON.stringify(err) );
+                logMsg("line:"+__line+", "+ JSON.stringify(err) );
                 return;
             }
             if (events != null) {
 
-                logMsg('Total ' + events.length + ' Events found');
+                logMsg("line:"+__line+", "+ 'Total ' + events.length + ' Events found');
 
+				//Those event id's , which has been started.
+				var startedEventIds = [];
+				var startedEventIdsCounter = 0;
+					
+					
                 // Loop through all record
                 for (var i = 0; i < events.length; i++) {
-
-                    logMsg('Start time : ' + new Date( Number(events[i].startTime) *1000 ));
-                    logMsg('End time   : ' + new Date( Number(events[i].endTime) *1000));
-                    
-                    if ( events[i].postSocialStatus != true && events[i].socialStatus != "") {
-						
-                            if ( events[i].fbAuth != ""  &&  events[i].fbAuthExpiryTs != "" ) {
-                                postOnFacebook( events[i].socialStatus, events[i].fbAuth, events[i].fbAuthExpiryTs );
-                            } 
-							
-                            if ( events[i].twitterAuth != "" && events[i].twOAuthTokenSecret != "" ) {                            
-								postOnTwitter(events[i].socialStatus, events[i].twitterAuth, events[i].twOAuthTokenSecret, function( ret ) {
-									logMsg( ret );
-								});
-                            } 
-						
-															
-                            if ( events[i].linkedinAuth != "" ) {
-                                postOnlinkedIn( events[i].socialStatus, events[i].linkedinAuth );
-                            } 
-                    }
 					
-					events[i].postSocialStatus = true;
+                    if ( events[i].postSocialStatus != true ) {
+						
+						logMsg( "line:"+__line+ ", postSocialStatus= "+ events[i].postSocialStatus );
+						
+						startedEventIds[ startedEventIdsCounter ] = {_id: events[i]._id};
+						startedEventIdsCounter++;
+						
+						events[i].socialStatus+", curTimestamp="+curTimestamp;
+						/*
+						postOnFacebook( events[i], events[i].socialStatus, events[i].fbAuth, events[i].fbAuthExpiryTs );						
+						postOnTwitter( events[i], events[i].socialStatus, events[i].twitterAuth, events[i].twOAuthTokenSecret, logMsg);						
+						postOnlinkedIn( events[i], events[i].socialStatus, events[i].linkedinAuth );
+						*/
+						
+                    	Events.findByIdAndUpdate(events[i]._id, { postSocialStatus:true }, {}, function(err, numberAffected, rawResponse) {
+							logMsg( {line: __line, err:err });
+						}) // executes
+					}					
 					
                 } //end of for loop
-				
+
 				//mass update all events( becuae we have updated postSocialStatus to true )
+				if( startedEventIdsCounter > 0 && 0 ) {
+					logMsg( {line: __line, startedEventIdsCounter: startedEventIdsCounter, startedEventIds: startedEventIds} );
+					//{$or: startedEventIds },
+					Events
+					.update({$or: startedEventIds },
+						{$set:{postSocialStatus:true} },
+						{ multi: true },
+						function(err, numberAffected, rawResponse) {
+							logMsg( {line: __line, err:err, numberAffected: numberAffected, rawResponse:rawResponse });
+						}
+					);//event update					
+				}
 				
-				
-
             } else {
-                logMsg('No Events found');
+                logMsg("line:"+__line+", No Events found");
             }
-
         });
-
     } // end postStatusEvent function
-	
-	
 	
 	   
     // Offline facebook posting
-    function postOnFacebook( socialStatus, fbAuth, fbAuthExpiryTs ) {
+    function postOnFacebook( curEvent, socialStatus, fbAuth, fbAuthExpiryTs ) {
+		var eIdTxt = " (EventId: " + curEvent._id + ") ";
 		
-		if( fbAuthExpiryTs > curTimestamp ){
-			logMsg( "Fb Token expired: "+ events[i].fbAuthExpiryTs + " > " + curTimestamp);
+        if ( fbAuth == ""  ||  fbAuthExpiryTs != "" ) {
+            logMsg({line:__line,eIdTxt: eIdTxt, msg: "fbAuth and fbAuthExpiryTs shouldnot be empty!", fbAuth:fbAuth, fbAuthExpiryTs:fbAuthExpiryTs} );
+        }
+		else if( fbAuthExpiryTs > curTimestamp ){
+			logMsg("line:"+__line+", "+ eIdTxt+"Fb Token expired:"+events[i].fbAuthExpiryTs + " > " + curTimestamp);
 		}
 		else{
 			FB.setAccessToken(fbAuth);
@@ -123,99 +134,96 @@ SocialStatusCron.setup = function(app) {
 			FB.api('me/feed', 'post', { message: body}, function (res2) {
 			
 			  if(!res2 || res2.error) {
-				  var msg = (!res2) ? "Fb posting error occurred." : ({a:"Fb posting error occurred: ", b:res2.error});
+				  var msg = (!res2) ? ({a:"Fb posting error occurred."}) : ({a:"Fb posting error occurred: ", b:res2.error});
+				  msg.eidTxt = eIdTxt;
 			  }
 			  else{
 				  var msg = 'Fb Post Id: ' + res2.id;
 			  }		  
-			  logMsg( msg );
+			  logMsg("line:"+__line+ msg );
 			});
 		}
     }//fb post function end
     
     // Offline twitter posting
-	function postOnTwitter(str, access_token_key, access_token_secret, callBack) {
+	function postOnTwitter( curEvent, str, access_token_key, access_token_secret, callBack) {
+		var eIdTxt = " (EventId: "+curEvent._id+") ";
+        if ( access_token_key == "" || access_token_secret == "" ) {                            
+			logMsg({line:__line, eIdTxt:eIdTxt, msg: "twitterAuth and twOAuthTokenSecret shouldnot be empty!", twitterAuth:access_token_key, twOAuthTokenSecret:access_token_secret} );
+        }
+		else {
+			var twit = new twitter({
+			  consumer_key: config.twitter.consumer_key,
+			  consumer_secret: config.twitter.consumer_secret,
 
-		var twit = new twitter({
-		  consumer_key: config.twitter.consumer_key,
-		  consumer_secret: config.twitter.consumer_secret,
+			  access_token_key: access_token_key,
+			  access_token_secret: access_token_secret
+			});
 
-		  access_token_key: access_token_key,
-		  access_token_secret: access_token_secret
-		});
+		  	twit.verifyCredentials(function (err, data) {
 
-	  	twit.verifyCredentials(function (err, data) {
-
-	        if (err) {
-	          callBack("Twitter Error verifying credentials: " + err);
+		        if ( err ) {
+		          callBack(eIdTxt+"Twitter Error verifying credentials: " + err);
   
-	        } else {
-	          twit.updateStatus(str, function (err, data) {
+		        } else {
+		          twit.updateStatus(str, function (err, data) {
   
-	                if (err) {
-	                  callBack('Tweeting failed: ' + err);
-	                } else {
-	                  callBack('Twitter Success!')
-	                }
-	          });
-	        }
+		                if (err) {
+		                  callBack(eIdTxt+"Tweeting failed:"+ err);
+		                } else {
+						  callBack("Twitter Success!");
+		                }
+		          });
+		        }
 		
-		});
-	
+			});
+		}	
 	}//twitter post fn end
 	
 	
-    function postOnlinkedIn2(str, linkedinAccessToken ){
-		logMsg( "In postOnlinkedIn");
+    function postOnlinkedIn( curEvent, str, linkedinAccessToken ) {
+		var eIdTxt = " (EventId: "+curEvent._id+") ";
+        if ( linkedinAccessToken == "" ) {
+            logMsg({line:__line, eIdTxt: eIdTxt, msg: "linkedinAccessToken shouldnot be empty!", linkedinAccessToken:linkedinAccessToken} ); 
+        }
+		else{		
+			var body = '<share>';
+				    body += '<comment>'+str+'</comment>';
+				    body += '<visibility>';
+						body += '<code>anyone</code>';
+					body += '</visibility>';
+				body += '</share>';
+			
+			var postRequest = {
+				host: 'api.linkedin.com',
+				port: 443,
+				path: '/v1/people/~/shares?oauth2_access_token=' + linkedinAccessToken,
+				method: "POST",
+			    headers: {
+			        'Cookie': "cookie",
+			        'Content-Type': 'text/xml',
+			        'Content-Length': Buffer.byteLength(body)
+			    }
+			};
 		
-		var body = '<share>';
-			    body += '<comment>'+str+'</comment>';
-			    body += '<visibility>';
-					body += '<code>anyone</code>';
-				body += '</visibility>';
-			body += '</share>';				
-			
-			
-		var postRequest = {
-			host: 'api.linkedin.com',
-			port: 443,
-			path: '/v1/people/~/shares?oauth2_access_token=' + linkedinAccessToken,
-			method: "POST",
-		    headers: {
-		        'Cookie': "cookie",
-		        'Content-Type': 'text/xml',
-		        'Content-Length': Buffer.byteLength(body)
-		    }
-		};
-
-			
-			
-		var buffer = "";
-
-		var req = https.request( postRequest, function( res )    {
-
-		   console.log( res.statusCode );
-		   var buffer = "";
-		   res.on( "data", function( data ) { buffer = buffer + data; } );
-		   res.on( "end", function( data ) { console.log( buffer ); } );
-
-		});
-
-		req.write( body );
-		req.end();
+			var buffer = "";
 		
+			var req = https.request( postRequest, function( res ) {
+	   
+			   logMsg("line:"+__line+", LinkedIn statusCode: "+ res.statusCode );		   
+		   
+			   buffer = "";
+	   
+			   res.on( "data", function( data ) { buffer = buffer + data; });
+			   res.on( "end",  function( data ) { logMsg("line:"+__line+", "+eIdTxt+ "LinkedIn  buffer: " + buffer ); });
+			});
+
+			req.write( body );
+			req.end();
+		}
     }//linkedin post function end
-	
-	
 
-	
-	postOnlinkedIn("Test1234", "AQUyRer11RBwK8mE2nqFWl2sYZq5NhgPTn1-J56C-lntwuRHvVBWPZWYrbroRfnUTuyAVPSlqWB2W-NEeYib6W-U8XT70UF2LQ7npgObhha8sylwUH7sfeduWhCMyTM9yR7zc5I-upOfhiwHnN03ECGD3YLSi8wW4qbgneOl3omfz8jhFI8" );
-	
-	
-		
-	
-	
-	
+	postStatusEvent();
 	
 	// TESTING CODE  ----------------{-------	
 	/*
