4a60457a21ff29da3ddc2814853b641c31035263
riksof-arbab <muhammad.arbab@riksof.com>
2014-12-23 19:21:03 +0500

    added new controllers files

---
+++ Untechable/ContactListCell.m
@@ -0,0 +1,23 @@
+//
+//  ContactListCell.m
+//  Untechable
+//
+//  Created by RIKSOF Developer on 12/23/14.
+//  Copyright (c) 2014 Green MTN Labs Inc. All rights reserved.
+//
+
+#import "ContactListCell.h"
+
+@implementation ContactListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
