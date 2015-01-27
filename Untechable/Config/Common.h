5352011ec7cdca3e25c2475c632b74438b8636e6
riksof-arbab <abdul.rauf@riksof.com>
2015-01-27 12:26:17 +0500

    change the untechableApi link to production

--- Untechable/Config/Common.h
+++ Untechable/Config/Common.h
@@ -47,9 +47,9 @@
 #define PRODUCTION @"PRODUCTION"
 
 
-#define SERVER_URL @"http://192.168.0.15:3000" //LOCALHOST
+//#define SERVER_URL @"http://192.168.0.15:3000" //LOCALHOST
 //#define SERVER_URL @"http://riksof.com:8000"    //DEVELOPMENT
-//#define SERVER_URL @"http://app.untechable.com:3010"   //PRODUCTION
+#define SERVER_URL @"http://app.untechable.com:3010"   //PRODUCTION
 
 #define API_GET_NUMBER SERVER_URL@"/get-forwading-number"
 #define API_SAVE SERVER_URL@"/event/save"
