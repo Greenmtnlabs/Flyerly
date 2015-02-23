2d47a211772c15e93b069c5c0636727dd3997c67
riksof-raheel <raheel.mateen@riksof.com>
2015-02-23 13:52:03 +0500

    Set the iphone6 check

--- eyeSpot/ESConstants.h
+++ eyeSpot/ESConstants.h
@@ -45,5 +45,12 @@ static NSString * const kESEventTagForApplicationError = @"Application Error";
 #pragma mark - Universal
 #define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 #define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-#define IS_IPHONE5 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 568.0f)
+
+#define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
+#define SCREEN_MAX_LENGTH (MAX(SCREEN_WIDTH, SCREEN_HEIGHT))
+#define SCREEN_MIN_LENGTH (MIN(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#define IS_IPHONE5 (IS_IPHONE && SCREEN_MAX_LENGTH == 568.0)
+#define IS_IPHONE6 (IS_IPHONE && SCREEN_MAX_LENGTH == 667.0)
 #endif
