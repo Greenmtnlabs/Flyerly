2e99ad04c4cf0c7f47c482a5a971c386881d7859
riksof-farhan <farhan.khan@riksof.com>
2015-05-11 11:44:52 +0500

    comments

--- Untechable/AddUntechable/AddUntechableController/AddUntechableController.m
+++ Untechable/AddUntechable/AddUntechableController/AddUntechableController.m
@@ -416,7 +416,7 @@
         
         if( [untechable.commonFunctions date1IsSmallerThenDate2:endD date2:statD] ){
             endD = [statD dateByAddingTimeInterval:(60*60*24)];
-            untechable.endDate = [untechable.commonFunctions nsDateToTimeStampStr:endD]; //current time +2hr
+            untechable.endDate = [untechable.commonFunctions nsDateToTimeStampStr:endD]; //current time +1 day
             NSString *dateStrUpdated = [untechable.dateFormatter stringFromDate:endD];
             [_btnEndTime setTitle:dateStrUpdated forState:UIControlStateNormal];
         }
@@ -650,7 +650,7 @@
     //[self showHideDateTimePicker:!([_cbNoEndDate isSelected])];
     
     if( !([_cbNoEndDate isSelected]) ){
-        untechable.endDate  = [untechable.commonFunctions nsDateToTimeStampStr: [[NSDate date] dateByAddingTimeInterval:(60*60*24)] ]; //current time +2hr
+        untechable.endDate  = [untechable.commonFunctions nsDateToTimeStampStr: [[NSDate date] dateByAddingTimeInterval:(60*60*24)] ]; //current time +1 Day
     }
     [self pickerSetAcTo:@"_btnEndTime"];
 }
