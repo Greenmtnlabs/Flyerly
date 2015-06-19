907191ca8b002e7912435a8afe63afba63b3f425
riksof-farhan <farhan.khan@riksof.com>
2015-06-19 15:16:38 +0500

    second screen added

---
+++ Untechable/SetupGuideSecondViewController.m
@@ -0,0 +1,30 @@
+//
+//  SetupGuideSecondViewController.m
+//  Untechable
+//
+//  Created by RIKSOF Developer on 6/19/15.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import "SetupGuideSecondViewController.h"
+
+@interface SetupGuideSecondViewController ()
+
+@end
+
+@implementation SetupGuideSecondViewController
+
+@synthesize untechable;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
