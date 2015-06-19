6ce925b0cbb4511454b9ced563500ca9df22816e
riksof-farhan <farhan.khan@riksof.com>
2015-06-19 08:37:20 +0500

    commented code

---
+++ Untechable/SetupGuideViewController.m
@@ -0,0 +1,37 @@
+//
+//  SetupGuideViewController.m
+//  Untechable
+//
+//  Created by RIKSOF Developer on 6/19/15.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import "SetupGuideViewController.h"
+
+@interface SetupGuideViewController ()
+
+@end
+
+@implementation SetupGuideViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
