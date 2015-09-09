5bb36701f0a1c4c82de89451f8e19c52638f1aea
riksof-arqam <hamza.ahmed@riksof.com>
2015-09-09 20:20:12 +0500

    work in progress

---
+++ Classes/IntroScreenViewController.h
@@ -0,0 +1,14 @@
+//
+//  IntroScreenViewController.h
+//  Flyr
+//
+//  Created by rufi on 09/09/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntroScreenViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
