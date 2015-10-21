ad85ac366dc55f89c6e56aec6efc7cb503ac119f
riksof-arqam <muhammad.arqam@riksof.com>
2015-10-21 15:07:18 +0500

    applied localization

--- Untechable/AddUntechable/AddUntechableController/AddUntechableController.m
+++ Untechable/AddUntechable/AddUntechableController/AddUntechableController.m
@@ -485,7 +485,6 @@
     
     [_btnLblWwud setTitle:NSLocalizedString(@"This is what I will be doing when I untech...", nil) forState:normal];
     [_btnLblWwud setTitleColor:DEF_GRAY forState:UIControlStateNormal];
-    _btnLblWwud.titleLabel.font = [UIFont fontWithName:APP_FONT size:25];
  
     _inputSpendingTimeText.text = untechable.spendingTimeTxt;
     if ( [untechable.spendingTimeTxt isEqualToString:@""] ){
@@ -495,18 +494,16 @@
     }
     _inputSpendingTimeText.font = [UIFont fontWithName:APP_FONT size:18];
     
-    [_btnLblStartTime setTitle:NSLocalizedString(@"Start", nil) forState:normal];
+    [_btnLblStartTime setTitle:NSLocalizedString(@"Start (when will you begin your untech time?)", nil) forState:normal];
     [_btnLblStartTime setTitleColor:DEF_GRAY forState:UIControlStateNormal];
-    _btnLblStartTime.titleLabel.font = [UIFont fontWithName:APP_FONT size:25];
     
     [_btnStartTime setTitleColor:DEF_GREEN forState:UIControlStateNormal];
     _btnStartTime.titleLabel.font = [UIFont fontWithName:APP_FONT size:18];
     [_btnStartTime setTitle:[untechable.commonFunctions convertTimestampToAppDate:untechable.startDate] forState:UIControlStateNormal];
     
     
-    [_btnLblEndTime setTitle:NSLocalizedString(@"End", nil) forState:normal];
+    [_btnLblEndTime setTitle:NSLocalizedString(@"End (this is when you come crawling back to technology)", nil) forState:normal];
     [_btnLblEndTime setTitleColor:DEF_GRAY forState:UIControlStateNormal];
-    _btnLblEndTime.titleLabel.font = [UIFont fontWithName:APP_FONT size:25];
     
     [_btnEndTime setTitleColor:DEF_GREEN forState:UIControlStateNormal];
     _btnEndTime.titleLabel.font = [UIFont fontWithName:APP_FONT size:18];
