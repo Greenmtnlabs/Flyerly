d9784f04b2f1f37ac92297dd01fa88e65bf1e065
riksof-arqam <muhammad.arqam@riksof.com>
2015-11-02 17:54:00 +0500

    added AdMobCell

---
+++ Classes/AdMobCell.h
@@ -0,0 +1,13 @@
+//
+//  AdMobCell.h
+//  Flyr
+//
+//  Created by rufi on 02/11/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdMobCell : UITableViewCell
+
+@end
---
+++ Classes/AdMobCell.m
@@ -0,0 +1,23 @@
+//
+//  AdMobCell.m
+//  Flyr
+//
+//  Created by rufi on 02/11/2015.
+//
+//
+
+#import "AdMobCell.h"
+
+@implementation AdMobCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
