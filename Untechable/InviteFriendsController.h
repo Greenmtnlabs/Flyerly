cdd930b571901a01fc468f92a6d5d0c7477218bb
riksof-arqam <muhammad.arqam@riksof.com>
2015-11-05 11:01:44 +0500

    added AppInvite screen and layout

---
+++ Untechable/InviteFriendsController.h
@@ -0,0 +1,18 @@
+//
+//  InviteFriendsController.h
+//  Untechable
+//
+//  Created by rufi on 05/11/2015.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InviteFriendsController : UIViewController
+
+@end
+
+
+
+
+
---
+++ Untechable/InviteFriendsController.m
@@ -0,0 +1,29 @@
+//
+//  InviteFriendsController.m
+//  Untechable
+//
+//  Created by rufi on 05/11/2015.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import "InviteFriendsController.h"
+
+@interface InviteFriendsController ()
+
+@end
+
+@implementation InviteFriendsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
