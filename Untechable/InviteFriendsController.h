a8905691a371ba2b41b247b9c67ff99005adf785
riksof-arqam <muhammad.arqam@riksof.com>
2015-11-05 12:12:58 +0500

    added delegates

--- Untechable/InviteFriendsController.h
+++ Untechable/InviteFriendsController.h
@@ -8,7 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface InviteFriendsController : UIViewController
+@interface InviteFriendsController : UIViewController<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate>
+
+
+@property (strong, nonatomic) IBOutlet UIButton *btnLocalContactsText;
+
+@property (strong, nonatomic) IBOutlet UIButton *btnFBContacts;
+
+@property (strong, nonatomic) IBOutlet UIButton *btnTwitterContacts;
+
+@property (strong, nonatomic) IBOutlet UIButton *btnLocalContactsEmail;
+
+@property (strong, nonatomic) IBOutlet UITextField *txtSearch;
+
+@property (strong, nonatomic) IBOutlet UITableView *ContactsTableView;
+
 
 @end
 
