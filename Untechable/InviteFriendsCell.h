c7992ddd996e5d0b3902a9e5e0e7ea95ad48d88d
riksof-arqam <muhammad.arqam@riksof.com>
2015-11-05 11:58:28 +0500

    work in progress on app invite

--- Untechable/InviteFriendsCell.h
+++ Untechable/InviteFriendsCell.h
@@ -2,16 +2,21 @@
 //  InviteFriendsCell.h
 //  Untechable
 //
-//  Created by rufi on 05/11/2015.
+//  Created by M. Arqam Owais on 05/11/2015.
 //  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "ContactsModel.h"
 
 @interface InviteFriendsCell : UITableViewCell
+
 @property (strong, nonatomic) IBOutlet UIImageView *imgProfilePic;
 @property (strong, nonatomic) IBOutlet UILabel *lblName;
 @property (strong, nonatomic) IBOutlet UILabel *lblDescription;
-@property (strong, nonatomic) IBOutlet UIImageView *lblCheckBox;
+@property (strong, nonatomic) IBOutlet UIImageView *imgCheckBox;
+
+
+-(void)setCellObjects :(ContactsModel *)contactsModel :(int)status :(NSString *)tableName;
 
 @end
--- Untechable/InviteFriendsCell.m
+++ Untechable/InviteFriendsCell.m
@@ -8,16 +8,48 @@
 
 #import "InviteFriendsCell.h"
 
+@interface InviteFriendsCell ()
+
+@end
+
 @implementation InviteFriendsCell
 
+@synthesize imgProfilePic, lblName, lblDescription, imgCheckBox;
+
 - (void)awakeFromNib {
-    // Initialization code
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
     // Configure the view for the selected state
 }
+
+-(void)setCellObjects :(ContactsModel *)contactsModel :(int)status :(NSString *)tableName{
+    
+    // Set Values
+    [lblDescription setText:contactsModel.name];
+    
+    if ([contactsModel.others isEqualToString:@""]){
+        [lblDescription setText:contactsModel.description];
+    }else {
+        [lblDescription setText:contactsModel.others];
+    }
+   
+    [imgProfilePic setImage:contactsModel.img];
+    contactsModel.delegate =(id)self;
+    if ( [tableName isEqualToString:@"InviteFriends"] ){
+        [contactsModel setInvitedStatus:status];
+    }else if ( [tableName isEqualToString:@"PrintInvites"] ){
+        [contactsModel setInvitedStatus:0];
+    }
+}
+
+#pragma mark Contacts  Delegate
+
+-(void)invitedContacts :(ContactsModel *)contactsModel{
+    
+    [imgCheckBox setImage:[UIImage imageNamed:contactsModel.checkImageName]];
+}
+
 
 @end
