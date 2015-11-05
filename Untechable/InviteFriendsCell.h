50b06cdd53debef6fba6353fdda4d85b2ae4b405
riksof-arqam <muhammad.arqam@riksof.com>
2015-11-05 11:46:30 +0500

    added invite friend cell

---
+++ Untechable/InviteFriendsCell.h
@@ -0,0 +1,13 @@
+//
+//  InviteFriendsCell.h
+//  Untechable
+//
+//  Created by rufi on 05/11/2015.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InviteFriendsCell : UITableViewCell
+
+@end
---
+++ Untechable/InviteFriendsCell.m
@@ -0,0 +1,23 @@
+//
+//  InviteFriendsCell.m
+//  Untechable
+//
+//  Created by rufi on 05/11/2015.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import "InviteFriendsCell.h"
+
+@implementation InviteFriendsCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
