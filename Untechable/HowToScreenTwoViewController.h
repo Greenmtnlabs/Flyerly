afa8c0481b8b2a1847f122baa59c2a9f3c221f6a
riksof-arqam <muhammad.arqam@riksof.com>
2015-12-03 19:25:48 +0500

    added screen 2

---
+++ Untechable/HowToScreenTwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  HowToScreenTwoViewController.h
+//  Untechable
+//
+//  Created by rufi on 03/12/2015.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HowToScreenTwoViewController : UIViewController
+
+@end
