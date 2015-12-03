6165655792c219057446454555bf7914a742954e
riksof-arqam <muhammad.arqam@riksof.com>
2015-12-03 18:59:14 +0500

    added how to screens

---
+++ Untechable/HowToScreenOneViewController.m
@@ -0,0 +1,37 @@
+//
+//  HowToScreenOneViewController.m
+//  Untechable
+//
+//  Created by rufi on 03/12/2015.
+//  Copyright (c) 2015 Green MTN Labs Inc. All rights reserved.
+//
+
+#import "HowToScreenOneViewController.h"
+
+@interface HowToScreenOneViewController ()
+
+@end
+
+@implementation HowToScreenOneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
