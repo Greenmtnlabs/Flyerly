9ed85032f9bed8032892e271dbbaece96d4a921a
riksof-arqam <muhammad.arqam@riksof.com>
2015-12-08 14:23:57 +0500

    fixed

--- Classes/CreateFlyerController.m
+++ Classes/CreateFlyerController.m
@@ -3212,6 +3212,10 @@ fontBorderTabButton,addVideoTabButton,addMorePhotoTabButton,addArtsTabButton,sha
     [self.navigationItem setLeftBarButtonItem:backBarButton];
     //--- Set navigation ----------- } --
     
+    // disable user interaction of Home and Help button when editing
+    backButton.enabled = NO;
+    helpButton.enabled = NO;
+    
     //Add Context Library
     [self addBottomTabs:libText];
     
@@ -3273,14 +3277,10 @@ fontBorderTabButton,addVideoTabButton,addMorePhotoTabButton,addArtsTabButton,sha
     NSMutableArray  *barItems   =   [NSMutableArray arrayWithObjects:nil];
     
     // enable user interaction of Home and Help button in every condition
-    backButton.userInteractionEnabled = YES;
-    helpButton.userInteractionEnabled = YES;
+    backButton.enabled = YES;
+    helpButton.enabled = YES;
     
     if( [callFrom isEqualToString:@"callWrite"] ){
-        
-        // disable user interaction of Home and Help button when editing
-        backButton.userInteractionEnabled = NO;
-        helpButton.userInteractionEnabled = NO;
         
         UIButton *nextButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 45, 42)];
         [nextButton addTarget:self action:@selector(nextForFont) forControlEvents:UIControlEventTouchUpInside];
