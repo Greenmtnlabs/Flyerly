98ff75c21d596309fe31e1246579ae791359e70d
riksof-arqam <muhammad.arqam@riksof.com>
2015-12-17 12:50:20 +0500

    work in progress for FB 4

--- server/providers/PostSocialStatusCron.js
+++ server/providers/PostSocialStatusCron.js
@@ -415,12 +415,8 @@ SocialStatusCron.setup = function(app) {
 			// });
 
 			var params = {};
-				params['message'] = 'The message';
-				params['name'] = 'Name';
-				params['description'] = 'this is a description';
-				params['link'] = 'http://www.somelink.com/page.htm';
-				params['picture'] = 'http://www.somelink.com/img/pic.jpg';
-				params['caption'] = 'Caption of the Post';
+				params['message'] = str;
+				params['picture'] = imagePath;
 				 
 				FB.api('/me/feed', 'post', params, function(response) {
 				  if (!response || response.error) {
