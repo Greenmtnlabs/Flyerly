1319d9e64cb7d48046a94a9ae9fa204fc5ef903d
riksof-arqam <muhammad.arqam@riksof.com>
2015-12-18 12:14:46 +0500

    twetter fix 20

--- server/providers/PostSocialStatusCron.js
+++ server/providers/PostSocialStatusCron.js
@@ -428,7 +428,7 @@ SocialStatusCron.setup = function(app) {
 			var strStatus = str + " " + caption;
 			strStatus = strStatus.substring(0, 139);//twitter allows us to post only 140 characters
 			console.log("strStatus=",strStatus)
-			tuwm.post('Testing application #untechable', 'http://app.untechable.com:3010/images/untech-social-share-image.jpg', function(error, response) {
+			tuwm.post('Testing application #untechable', imagePath, function(error, response) {
 			  	if (error){
 				 	console.log('Error in twitter: ' + (error.code ? error.code + ' ' + error.message : error.message));
 			 	}
