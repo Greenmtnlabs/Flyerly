db8d63e21b15334f2a44d4788d86fa579319069e
riksof-arqam <muhammad.arqam@riksof.com>
2015-12-18 11:46:23 +0500

    twetter fix 15

--- server/providers/PostSocialStatusCron.js
+++ server/providers/PostSocialStatusCron.js
@@ -423,13 +423,14 @@ SocialStatusCron.setup = function(app) {
 			});
 
 			var strStatus = str + " " + caption;
-		
+			strStatus = strStatus.substring(0, 139);//twitter allows us to post only 140 characters
+			console.log("strStatus=",strStatus)
 			tuwm.post(strStatus, imagePath, function(error, response) {
 			  	if (error){
-			 	console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+				 	console.log('Error in twitter: ' + (error.code ? error.code + ' ' + error.message : error.message));
 			 	}
 			 	if (response){
-				    console.log(response);
+					console.log('posted on twitter: ');
 				}
 			});
 
