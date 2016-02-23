dd59f0e2948ce477c3486f87f67ae264a8591b98
riksof-arqam <muhammad.arqam@riksof.com>
2016-02-23 20:09:25 +0500

    working on add untechable controller

--- Untechable/AddUntechable/AddUntechableController/AddUntechableController.m
+++ Untechable/AddUntechable/AddUntechableController/AddUntechableController.m
@@ -377,19 +377,19 @@
     }
     [self.spendingTimeTextPicker selectRow:positionToShow inComponent:0 animated:NO];
 
-    if ( IS_IPHONE_4 ){
-        [_pickerCloseBtn setFrame:CGRectMake(-2, 300, 580, 30)];
-        [_spendingTimeTextPicker setFrame:CGRectMake(0, 300, 0, 140)];
-    }else if( IS_IPHONE_5 ){
-        [_pickerCloseBtn setFrame:CGRectMake(-10, 340, 580, 35)];
-        [_spendingTimeTextPicker setFrame:CGRectMake(0, 375, 0, 240)];
-    }else if ( IS_IPHONE_6 ){
-        [_pickerCloseBtn setFrame:CGRectMake(0, 400, 650, 40)];
-        [_spendingTimeTextPicker setFrame:CGRectMake(0, 440, 0, 260)];
-    }else if (IS_IPHONE_6_PLUS){
-        [_pickerCloseBtn setFrame:CGRectMake(0, 500, 750, 50)];
-        [_spendingTimeTextPicker setFrame:CGRectMake(0, 540, 0, 500)];
-    }
+//    if ( IS_IPHONE_4 ){
+//        [_pickerCloseBtn setFrame:CGRectMake(-2, 300, 580, 30)];
+//        [_spendingTimeTextPicker setFrame:CGRectMake(0, 300, 0, 140)];
+//    }else if( IS_IPHONE_5 ){
+//        [_pickerCloseBtn setFrame:CGRectMake(-10, 340, 580, 35)];
+//        [_spendingTimeTextPicker setFrame:CGRectMake(0, 375, 0, 240)];
+//    }else if ( IS_IPHONE_6 ){
+//        [_pickerCloseBtn setFrame:CGRectMake(0, 400, 650, 40)];
+//        [_spendingTimeTextPicker setFrame:CGRectMake(0, 440, 0, 260)];
+//    }else if (IS_IPHONE_6_PLUS){
+//        [_pickerCloseBtn setFrame:CGRectMake(0, 500, 750, 50)];
+//        [_spendingTimeTextPicker setFrame:CGRectMake(0, 540, 0, 500)];
+//    }
     
     float alpha = (showHide) ? 1.0 : 0.0;
     
@@ -407,19 +407,19 @@
 
 -(void)showHideDateTimePicker:(BOOL)showHide{
 
-    if ( IS_IPHONE_4 ){
-        [_pickerCloseBtn setFrame:CGRectMake(-2, 300, 580, 30)];
-        [_picker setFrame:CGRectMake(0, 310, 0, 140)];
-    }else if( IS_IPHONE_5 ){
-        [_pickerCloseBtn setFrame:CGRectMake(-10, 340, 580, 35)];
-        [_picker setFrame:CGRectMake(0, 360, 0, 260)];
-    }else if ( IS_IPHONE_6 ){
-        [_pickerCloseBtn setFrame:CGRectMake(0, 430, 650, 40)];
-        [_picker setFrame:CGRectMake(0, 440, 0, 260)];
-    }else if (IS_IPHONE_6_PLUS){
-        [_pickerCloseBtn setFrame:CGRectMake(0, 500, 750, 45)];
-        [_picker setFrame:CGRectMake(0, 510, 0, 500)];
-    }
+//    if ( IS_IPHONE_4 ){
+//        [_pickerCloseBtn setFrame:CGRectMake(-2, 300, 580, 30)];
+//        [_picker setFrame:CGRectMake(0, 310, 0, 140)];
+//    }else if( IS_IPHONE_5 ){
+//        [_pickerCloseBtn setFrame:CGRectMake(-10, 340, 580, 35)];
+//        [_picker setFrame:CGRectMake(0, 360, 0, 260)];
+//    }else if ( IS_IPHONE_6 ){
+//        [_pickerCloseBtn setFrame:CGRectMake(0, 430, 650, 40)];
+//        [_picker setFrame:CGRectMake(0, 440, 0, 260)];
+//    }else if (IS_IPHONE_6_PLUS){
+//        [_pickerCloseBtn setFrame:CGRectMake(0, 500, 750, 45)];
+//        [_picker setFrame:CGRectMake(0, 510, 0, 500)];
+//    }
     
     lblQoute.hidden = showHide;
     float alpha = (showHide) ? 1.0 : 0.0;
