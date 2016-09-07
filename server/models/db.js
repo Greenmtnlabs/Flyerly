9d792b8cac3bd730f128ea81f7173774fafab11e
root <root@ip-172-31-38-103.ec2.internal>
2016-09-07 13:20:54 +0000

    fixed mongo topology error

--- server/models/db.js
+++ server/models/db.js
@@ -7,4 +7,8 @@ var config = require( __dirname + '/../config' );
 
 // Reference to the module to be exported
 db = module.exports = require( 'mongoose' );
-db.connect( config.db.host );
+var options = {
+  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+};
+db.connect( config.db.host, options );
